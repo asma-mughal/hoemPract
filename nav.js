9a46ec90203c0609d3ec5f27fab8492fbbd14f39
unknown <asmamughal097@gmail.com>
2024-03-02 19:14:58 +0500

    work

--- nav.js
+++ nav.js
@@ -2,14 +2,20 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 app.get("/", (req, res)=>{
-    res.send("this is homePage")
+    res.write("<h1>This is my work</h1>")
+    //res.send("<h1>this is homePage</h1>")
+    res.send()
 })
 app.get("/about", (req, res)=>{
     //res.send("this is About")
     res.status(200).send("this is About Page")
 })
 app.get("/contact", (req, res)=>{
-    res.send("this is Contact Page")
+    //res.send("this is Contact Page")
+    res.send({
+        id:1,
+        name:'Jahil'
+    })
 })
 app.listen(PORT, ()=>{
     console.log(`listening to this ${PORT}`)
